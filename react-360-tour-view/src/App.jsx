0f178b178bdbe83855fa812d9039ce0dd1c3ceaf
Nông Quốc Khánh <hkg11khanh@gmail.com>
2025-03-09 00:33:19 +0700

    init repo

---
+++ react-360-tour-view/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import View360 from './components/View360';
+
+function App() {
+  return (
+    <><h1>HEllo ươrld</h1>
+    <View360></View360></>
+  );
+}
+
+export default App;
---
+++ react-360-tour-view/src/components/View360.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef } from "react";
+
+export default function View360() {
+    const panoRef = useRef(null);
+
+    useEffect(() => {
+        
+    }, []);
+
+    return (
+        <></>
+    );
+}
